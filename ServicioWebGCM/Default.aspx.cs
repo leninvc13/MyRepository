b8d434c9dd6559df8f9a4fac039d4b670643b481
leninvc13 <leninvalencia@hotmail.com>
2015-10-28 11:01:08 -0600

    First commit test

---
+++ ServicioWebGCM/Default.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.IO;
+
+namespace ServicioWebGCM
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGCM_Click(object sender, EventArgs e)
+        {
+
+
+
+            ServicioClientes svc = new ServicioClientes();
+            User codUsuario = svc.CodigoUsuario((txtUsuario.Text));
+            bool res = enviarMensajePrueba(codUsuario.CodigoC2DM);
+
+            if (res == true)
+                lblEnvio.Text = "Envio OK";
+            else
+                lblEnvio.Text = "Envio NO oK";
+        }
+
+
+        private static bool enviarMensajePrueba(String registration_id)
+        {
+            bool flag = false;
+            StringBuilder sb = new StringBuilder();
+
+            String GCM_URL = @"https://android.googleapis.com/gcm/send";
+
+            string collapseKey = DateTime.Now.ToString();
+
+            sb.AppendFormat("registration_id={0}&collapse_key={1}",
+                            registration_id, collapseKey);
+
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            data.Add("data.msg",
+                HttpUtility.UrlEncode("Prueba. Timestamp: " + DateTime.Now.ToString()));
+
+            foreach (string item in data.Keys)
+            {
+                if (item.Contains("data."))
+                    sb.AppendFormat("&{0}={1}", item, data[item]);
+            }
+
+            string msg = sb.ToString();
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(GCM_URL);
+            req.Method = "POST";
+            req.ContentLength = msg.Length;
+            req.ContentType = "application/x-www-form-urlencoded";
+
+            string apiKey = "AIzaSyCRLEpdWf9PNAdiNRv4pRrh_Qp_WSe5vSI";
+            req.Headers.Add("Authorization:key=" + apiKey);
+
+            using (StreamWriter oWriter = new StreamWriter(req.GetRequestStream()))
+            {
+                oWriter.Write(msg);
+            }
+
+            using (HttpWebResponse resp = (HttpWebResponse)req.GetResponse())
+            {
+                using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+                {
+                    string respData = sr.ReadToEnd();
+
+                    if (resp.StatusCode == HttpStatusCode.OK)   // OK = 200
+                    {
+                        //if (respData.StartsWith("id="))
+                            flag = true;
+                    }
+                    else if (resp.StatusCode == HttpStatusCode.InternalServerError)    // 500
+                        Console.WriteLine("Error interno del servidor, prueba más tarde.");
+                    else if (resp.StatusCode == HttpStatusCode.ServiceUnavailable)    // 503
+                        Console.WriteLine("Servidor no disponible temporalmente, prueba más tarde.");
+                    else if (resp.StatusCode == HttpStatusCode.Unauthorized)          // 401
+                        Console.WriteLine("La API Key utilizada no es válida.");
+                    else
+                        Console.WriteLine("Error: " + resp.StatusCode);
+                }
+            }
+
+            return flag;
+        }
+
+    }
+}
---
+++ ServicioWebGCM/ServicioClientes.asmx.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml.Serialization;
+
+namespace ServicioWebGCM
+{
+    /// <summary>
+    /// Summary description for ServicioClientes
+    /// </summary>
+    [WebService(Namespace = "http://mytest.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class ServicioClientes : System.Web.Services.WebService
+    {
+        DBCLIENTESEntities dataBase = new DBCLIENTESEntities();
+
+        [WebMethod]
+        public int Add(int a, int b)
+        {
+            return a + b;
+        } 
+
+
+        [WebMethod]
+        public int RegistroUsuario(string NombreUsuario, string CodigoC2DM)
+        {
+            User user = new User();
+
+            User usuar = CodigoUsuario(NombreUsuario);
+            int id = 0;
+            if (usuar == null)
+            {
+                user.NombreUsuario = NombreUsuario;
+                user.CodigoC2DM = CodigoC2DM;
+                dataBase.Users.Add(user);
+                dataBase.SaveChanges();
+                id = user.IdUsuario;
+            }
+            else
+            {
+                User u = dataBase.Users.First(i => i.IdUsuario == usuar.IdUsuario);
+                u.CodigoC2DM = CodigoC2DM;
+                dataBase.SaveChanges();
+                id = u.IdUsuario;
+            }
+
+
+
+            return id;
+            
+        }
+
+        [WebMethod]
+        public User CodigoUsuario(string NombreUsuario)
+        {
+
+            var us1 = (from u in dataBase.Users
+                       where u.NombreUsuario.Equals(NombreUsuario)
+                       select u).SingleOrDefault();
+
+            return us1;
+        }
+
+        //[WebMethod]
+        //public Cliente[] ListadoClientes()
+        //{
+        //    List<Cliente> lista = new List<Cliente>();
+        //    var clientList = dataBase.Clientes.ToList();
+        //    lista = clientList;
+
+        //    return lista.ToArray();
+
+        //}
+
+        //[WebMethod]
+        //public int NuevoClienteObjeto(Cliente cliente)
+        //{
+        //    Cliente mycliente = new Cliente();
+        //    mycliente.Nombre = cliente.Nombre;
+        //    mycliente.Telefono = cliente.Telefono;
+        //    dataBase.Clientes.Add(mycliente);
+        //    dataBase.SaveChanges();
+
+        //    int id = mycliente.IdCliente;
+
+        //    return id;
+        //}
+
+        //[WebMethod]
+        //public int NuevoClientesSimple(string nombre, int telefono)
+        //{
+        //    Cliente cliente = new Cliente();
+        //    cliente.Nombre = nombre;
+        //    cliente.Telefono = telefono;
+        //    dataBase.Clientes.Add(cliente);
+        //    dataBase.SaveChanges();
+
+        //    int id = cliente.IdCliente;
+
+        //    return id;
+        //}
+
+        //[WebMethod]
+        //public int RegistroUsuario(string usuario, string regGCM)
+        //{
+        //    User user = new User();
+
+        //    User usuar = CodigoUsuario(usuario);
+        //    int id = 0;
+        //    if (usuar == null)
+        //    {
+        //        user.NombreUsuario = usuario;
+        //        user.CodigoC2DM = regGCM;
+        //        dataBase.Users.Add(user);
+        //        dataBase.SaveChanges();
+        //        id = user.IdUsuario;
+        //    }
+        //    else
+        //    {
+        //        User u = dataBase.Users.First(i => i.IdUsuario == usuar.IdUsuario);
+        //        u.CodigoC2DM = regGCM;
+        //        dataBase.SaveChanges();
+        //        id = u.IdUsuario;
+        //    }
+
+
+
+        //    return id;
+
+        //}
+
+
+        //[WebMethod]
+        //public User CodigoUsuario(string usuario)
+        //{
+        //    var us1 = (from u in dataBase.Users
+        //               where u.NombreUsuario.Equals(usuario)
+        //               select u).SingleOrDefault();
+
+        //    return us1;
+        //}
+
+
+    }
+}
